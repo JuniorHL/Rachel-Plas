68dae6dc7b418efd311cb1ef5f88019f5b26233c
Anselmo Huancas <junior.huancas.leuyacc@gmail.com>
2022-11-01 18:20:11 -0500

    login con firebase

---
+++ RachelPlas/app/src/main/java/com/untels/rachelplas/Login.kt
@@ -0,0 +1,72 @@
+package com.untels.rachelplas
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.untels.rachelplas.databinding.ActivityLoginBinding
+
+class Login : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+
+        binding.Ingresar.setOnClickListener {
+            val mEmail = binding.Email.text.toString()
+            val mPassword = binding.Contra.text.toString()
+
+            when{
+                mEmail.isEmpty() || mPassword.isEmpty() -> {
+                    Toast.makeText(baseContext, "Correo o contraseña incorrectos",
+                        Toast.LENGTH_SHORT).show()
+                } else -> {
+                    InicioSesion(mEmail, mPassword)
+                }
+            }
+        }
+    }
+
+    //Verificar si la sesion ya esta inicializada
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private fun InicioSesion(email: String, password: String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "signInWithEmail:success")
+                    reload()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+
+                }
+            }
+    }
+
+    private fun reload(){
+        //nos lleva a la pantalla productos
+        val intent = Intent(this, MainActivity::class.java)
+        this.startActivity(intent)
+    }
+
+}
---
+++ RachelPlas/app/src/main/java/com/untels/rachelplas/RecuperarContra.kt
@@ -0,0 +1,11 @@
+package com.untels.rachelplas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecuperarContra : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recuperar_contra)
+    }
+}
